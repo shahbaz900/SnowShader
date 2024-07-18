d097e1c809db13cb8b598d95416a40629dc895be
Muhammad Shahbaz <l217525@lhr.nu.edu.pk>
2024-07-18 23:36:41 +0500

    snow effect+walking animation

--- Snow Shader 3D/Assets/Krjemelik/Snow Shader 3D/Player/Player.cs
+++ Snow Shader 3D/Assets/Krjemelik/Snow Shader 3D/Player/Player.cs
@@ -1,29 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private Transform _target;
     [SerializeField] private float _speed;
+    [SerializeField] private Animator _animator; // Reference to the Animator component
 
-    private void Start()
-    {
-        _target.transform.position =
-            new Vector3(_target.transform.position.x, transform.position.y, _target.transform.position.z);
-    }
-
-    void Update()
+    private void Update()
     {
         Move();
+        HandleAnimation();
     }
 
     protected virtual void Move()
     {
-        transform.position = Vector3.MoveTowards(
-            transform.position,
-            _target.transform.position,
-            _speed * Time.deltaTime);
+        // Get input from WASD keys
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        // Calculate the movement direction
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical).normalized;
+
+        // Move the player if there's any input
+        if (movement != Vector3.zero)
+        {
+            transform.position += movement * _speed * Time.deltaTime;
+        }
+    }
+
+    private void HandleAnimation()
+    {
+        // Check if there's any movement input
+        bool isWalking = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) ||
+                         Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D);
+
+        // Set the animator parameter based on whether the player is walking or not
+        _animator.SetBool("isWalking", isWalking);
     }
 }
